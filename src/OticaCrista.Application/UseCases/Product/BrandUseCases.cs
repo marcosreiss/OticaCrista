c1445f8e5e28880843f12d455ebaff19737bf974
Marcos <m.vinicius.sr11@gmail.com>
2024-05-26 17:01:45 -0300

    injeção de dependencia do brand use cases

--- src/OticaCrista.Application/UseCases/Product/BrandUseCases.cs
+++ src/OticaCrista.Application/UseCases/Product/BrandUseCases.cs
@@ -1,7 +1,14 @@
-﻿namespace OticaCrista.Application.UseCases.Product
+﻿using OticaCrista.Infra.DataBase.Repository;
+
+namespace OticaCrista.Application.UseCases.Product
 {
     public class BrandUseCases
     {
+        private readonly IBrandRepository _brandRepository;
 
+        public BrandUseCases(IBrandRepository brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
     }
 }
