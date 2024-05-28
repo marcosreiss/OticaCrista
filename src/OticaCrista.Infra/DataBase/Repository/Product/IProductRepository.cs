0da677bffeadd91e46f996c349705880138a5eeb
Marcos <m.vinicius.sr11@gmail.com>
2024-05-28 11:15:54 -0300

    metodo add implementado no product repository

--- src/OticaCrista.Infra/DataBase/Repository/Product/IProductRepository.cs
+++ src/OticaCrista.Infra/DataBase/Repository/Product/IProductRepository.cs
@@ -6,7 +6,7 @@ namespace OticaCrista.Infra.DataBase.Repository.Product
     {
         Task<List<ProductModel>> GetAll();
         Task<ProductModel> GetById(int id);
-        Task<ProductModel> Add();
+        Task<ProductModel> Add(ProductModel product);
         Task<ProductModel> Update(ProductModel model, int id);
         Task<bool> Delete(int id);
          
--- src/OticaCrista.Infra/DataBase/Repository/Product/ProductRepository.cs
+++ src/OticaCrista.Infra/DataBase/Repository/Product/ProductRepository.cs
@@ -1,12 +1,24 @@
-﻿using SistOtica.Models.Product;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using SistOtica.Models.Product;
 
 namespace OticaCrista.Infra.DataBase.Repository.Product
 {
     public class ProductRepository : IProductRepository
     {
-        public Task<ProductModel> Add()
+        private readonly IDbContextFactory<OticaCristaContext> _dbContextFactory;
+        public ProductRepository(IDbContextFactory<OticaCristaContext> context)
         {
-            throw new NotImplementedException();
+            _dbContextFactory = context;
+        }
+
+        public async Task<ProductModel> Add(ProductModel product)
+        {
+            using var context = _dbContextFactory.CreateDbContext();
+            await context.Products.AddAsync(product);
+            context.SaveChanges();
+            return product;
+           
         }
 
         public Task<bool> Delete(int id)
