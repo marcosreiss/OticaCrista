eb64eb6b4550a03b5a37235f367aab547c001b58
Marcos <m.vinicius.sr11@gmail.com>
2024-05-29 20:31:23 -0300

    create client endpoint criado

---
+++ src/OticaCrista.Api/Controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using OticaCrista.Application.UseCases.Client;
+using OticaCrista.communication.Requests.Client;
+using SistOtica.Models.Client;
+
+namespace OticaCrista.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] RequestClientJson requestClientJson,
+            [FromServices] CreateClientUseCase useCase)
+        {
+            var client = await useCase.Execute(requestClientJson);
+            return Created(string.Empty, client);
+        }
+    }
+}
--- src/OticaCrista.Api/Program.cs
+++ src/OticaCrista.Api/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddMvc();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
