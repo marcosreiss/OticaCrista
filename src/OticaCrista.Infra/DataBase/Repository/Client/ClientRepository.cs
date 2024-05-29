daf0513193408d8ed8c5cfe9489380ed25e0322e
Marcos <m.vinicius.sr11@gmail.com>
2024-05-29 09:05:58 -0300

    repository get all clients implementado

--- src/OticaCrista.Infra/DataBase/Repository/Client/ClientRepository.cs
+++ src/OticaCrista.Infra/DataBase/Repository/Client/ClientRepository.cs
@@ -1,32 +1,48 @@
-﻿using OticaCrista.communication.Requests.Client;
+﻿using Microsoft.EntityFrameworkCore;
+using OticaCrista.communication.Requests.Client;
 using SistOtica.Models.Client;
 
 namespace OticaCrista.Infra.DataBase.Repository.Client
 {
     public class ClientRepository : IClientRepository
     {
+        private readonly IDbContextFactory<OticaCristaContext> _contextFactory;
+        public ClientRepository(IDbContextFactory<OticaCristaContext> contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+
         public async Task<ClientModel> AddClient(RequestClientJson request)
         {
+            using var context = _contextFactory.CreateDbContext();
             throw new NotImplementedException();
         }
 
         public async Task<List<ClientModel>> GetAllClients()
         {
-            throw new NotImplementedException();
+            using var context = _contextFactory.CreateDbContext();
+            var clients = await context.Clients.ToListAsync();
+            return clients;
         }
 
         public async Task<ClientModel> GetClientById(int id)
         {
-            throw new NotImplementedException();
+            using var context = _contextFactory.CreateDbContext();
+
         }
 
         public async Task<ClientModel> UpdateClient(RequestClientJson request, int id)
         {
+            using var context = _contextFactory.CreateDbContext();
+
             throw new NotImplementedException();
         }
 
         public async Task<bool> DeleteClient(int id)
         {
+            using var context = _contextFactory.CreateDbContext();
+
             throw new NotImplementedException();
         }
 
@@ -34,33 +50,45 @@ namespace OticaCrista.Infra.DataBase.Repository.Client
 
         public async Task<bool> AddContact(ContactJson contact)
         {
+            using var context = _contextFactory.CreateDbContext();
+
             throw new NotImplementedException();
         }
 
         public async Task<bool> AddReference(ReferenceJson contact)
         {
+            using var context = _contextFactory.CreateDbContext();
+
             throw new NotImplementedException();
         }
 
 
         public async Task<bool> UpdateContact(ContactJson contact, int id)
         {
+            using var context = _contextFactory.CreateDbContext();
+
             throw new NotImplementedException();
         }
 
         public async Task<bool> UpdateReference(ReferenceJson contact, int id)
         {
+            using var context = _contextFactory.CreateDbContext();
+
             throw new NotImplementedException();
         }
 
 
         public async Task<bool> DeleteContact(int id)
         {
+            using var context = _contextFactory.CreateDbContext();
+
             throw new NotImplementedException();
         }
 
         public async Task<bool> DeleteReference(int id)
         {
+            using var context = _contextFactory.CreateDbContext();
+
             throw new NotImplementedException();
         }
     }
