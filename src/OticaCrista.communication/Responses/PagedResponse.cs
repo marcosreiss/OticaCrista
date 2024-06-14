4ac418823ca9d54ea46cbf80d49c3d2d1095bec5
Marcos <m.vinicius.sr11@gmail.com>
2024-06-13 22:11:22 -0300

    paged response criado

---
+++ src/OticaCrista.communication/Responses/PagedResponse.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OticaCrista.communication.Responses
+{
+    public class PagedResponse<TData> : Response<TData>
+    {
+        #region Ctor
+
+        [JsonConstructor]
+        public PagedResponse(
+            TData? data,
+            int totalCount,
+            int currentPage = 1,
+            int pageSize = 20)
+            : base(data)
+        {
+            TotalCount = totalCount;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+        }
+
+        #endregion
+
+        #region Props
+
+        public int CurrentPage { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+        public int PageSize { get; set; } = 20;
+        public int TotalCount { get; set; }
+
+        #endregion
+    }
+}
