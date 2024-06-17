99942cdfe8ce24b3d29d4b27555d8f0c0f87845d
Marcos <m.vinicius.sr11@gmail.com>
2024-06-16 23:10:10 -0300

    create client use case refatorado

--- src/OticaCrista.Application/UseCases/Client/CreateClientUseCase.cs
+++ src/OticaCrista.Application/UseCases/Client/CreateClientUseCase.cs
@@ -1,22 +1,33 @@
 ﻿using FluentValidation;
+using Microsoft.Extensions.Logging;
 using OticaCrista.Application.UseCases.Client.Validators;
 using OticaCrista.communication.Requests.Client;
+using OticaCrista.communication.Responses;
 using OticaCrista.Infra.DataBase.Repository.Client;
 using SistOtica.Models.Client;
 
 namespace OticaCrista.Application.UseCases.Client
 {
-    public class CreateClientUseCase(IClientRepository _repository)
+    public class CreateClientUseCase(
+        IClientRepository _repository,
+        ILogger<CreateClientUseCase> _logger)
     {
-        public async Task<ClientModel> Execute(ClientRequest request)
+        public async Task<Response<ClientModel>> Execute(ClientRequest request)
         {
-            await Validate(request);
+            try
+            {
+                await Validate(request);
+            }
+            catch (ValidationException ex)
+            {
+                return new Response<ClientModel>(null, 400, ex.Message);
+            }
             var client = await _repository.CreateClientAsync(request);
             if (client != null)
             {
-                return client;
+                return new Response<ClientModel>(client, 201, "Cliente cadastrado com sucesso!");
             }
-            return null;
+            return new Response<ClientModel>(null, 500, "Erro no repositório ao tentar cadastrar cliente, verifique os logs da aplicação");
         }
 
         private async Task Validate(ClientRequest request)
@@ -31,6 +42,7 @@ namespace OticaCrista.Application.UseCases.Client
                     var error = $"Property {failure.PropertyName} failed validation. Error was: {failure.ErrorMessage}\n";
                     errorMessages += error;
                 }
+                _logger.LogError("Erro em CreateClientUseCase.Validate:\n" + errorMessages);
                 throw new ValidationException(errorMessages);
             }
         }
