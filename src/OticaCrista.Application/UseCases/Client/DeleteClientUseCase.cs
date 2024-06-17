b54d14e4201f6c8118187e71edfcbba21e6c8a13
Marcos <m.vinicius.sr11@gmail.com>
2024-06-16 23:35:36 -0300

    Delete client usecase refatorado

--- src/OticaCrista.Application/UseCases/Client/DeleteClientUseCase.cs
+++ src/OticaCrista.Application/UseCases/Client/DeleteClientUseCase.cs
@@ -1,19 +1,19 @@
-﻿using OticaCrista.Infra.DataBase.Repository.Client;
+﻿using OticaCrista.communication.Responses;
+using OticaCrista.Infra.DataBase.Repository.Client;
+using SistOtica.Models.Client;
 
 namespace OticaCrista.Application.UseCases.Client
 {
-    public class DeleteClientUseCase
+    public class DeleteClientUseCase(IClientRepository repository)
     {
-        private readonly IClientRepository _clientRepository;
-        public DeleteClientUseCase(IClientRepository clientRepository)
+        public async Task<Response<ClientModel>> Execute(int id)
         {
-            _clientRepository = clientRepository;
-        }
-
-        public async Task<bool> Execute(int id)
-        {
-            if(await _clientRepository.DeleteClientAsync(id)) return true;
-            return false;
+            var client = await repository.DeleteClientAsync(id);
+            if (client != null)
+            {
+                return new Response<ClientModel>(client, 200, "Cliente deletado com Sucesso!");
+            }
+            return new Response<ClientModel>(null, 500, "Erro ao deletar client no repository. Verifique os logs da aplicação");
         }
     }
 }
